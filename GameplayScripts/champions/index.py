38f255e02abfc4d00893d9bd820ae6157d4eaeff
bckd00r <48347237+bckd00r@users.noreply.github.com>
2021-02-21 00:41:57 +0300

    Update index.py

--- GameplayScripts/champions/index.py
+++ GameplayScripts/champions/index.py
@@ -1,51 +1,10 @@
-from . import ashe
-from . import brand
-from . import draven
-from . import jinx
-from . import rengar
 from . import yasuo
-from . import zed
-from . import malphite
-from . import ryze
-from . import lucian
 
 def useComboToChampion(game):
-    if game.player.name == "ashe":
-        return ashe.Ashe().combo(game)
-    elif game.player.name == "brand":
-        return brand.Brand().combo(game)
-    elif game.player.name == "draven":
-        return draven.Draven().combo(game)
-    elif game.player.name == "jinx":
-        return jinx.Jinx().combo(game)
-    elif game.player.name == "rengar":
-        return rengar.Rengar().combo(game)
-    elif game.player.name == "yasuo":
-        return yasuo.Yasuo().combo(game)
-    elif game.player.name == "zed":
-        return zed.Zed().combo(game)
-    elif game.player.name == "malphite":
-        return malphite.Malphite().combo(game)
-    elif game.player.name == "ryze":
-        return ryze.Ryze().combo(game)
-    elif game.player.name == "lucian":
-        return lucian.Lucian().combo(game)
+    if game.player.name == "yasuo":
         
 def useHarrasMode(game):
-    if game.player.name == "ashe":
-        return ashe.Ashe().harras(game)
-    elif game.player.name == "brand":
-        return brand.Brand().harras(game)
-    elif game.player.name == "draven":
-        return draven.Draven().harras(game)
-    elif game.player.name == "jinx":
-        return jinx.Jinx().harras(game)
-    elif game.player.name == "rengar":
-        return rengar.Rengar().harras(game)
-    elif game.player.name == "yasuo":
-        return yasuo.Yasuo().harras(game)
-    elif game.player.name == "zed":
-        return zed.Zed().harras(game)
+    if game.player.name == "yasuo":
 
 def useEvadeToChampion(game, save_pos):
     if game.player.name == "yasuo":
